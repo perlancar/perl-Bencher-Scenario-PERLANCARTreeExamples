9405942c3bba912398461fd983bac8c58c7e837a
perlancar (@pc-home) <perlancar@gmail.com>
2016-03-26 22:02:53 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/PERLANCARTreeExamples.pm
@@ -0,0 +1,29 @@
+package Bencher::Scenario::PERLANCARTreeExamples;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark PERLANCAR::Tree::Examples',
+    description => <<'_',
+
+Created just for testing, while adding feature in `Bencher` to return result
+size.
+
+_
+    participants => [
+        {
+            fcall_template => 'PERLANCAR::Tree::Examples::gen_sample_tree(size => <size>, backend => <backend>)',
+        },
+    ],
+    datasets => [
+        {name => 'dataset', args=>{'size@'=>['tiny1', 'medium1'], 'backend@'=>['hash', 'array']}},
+    ],
+};
+
+1;
+# ABSTRACT:
